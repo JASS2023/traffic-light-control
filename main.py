969bea745c11d967c2abeed730bed7fa70e6ded5
Tobias Wasner <wasnertobias@users.noreply.github.com>
2023-03-29 23:14:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,6 @@ from time import sleep
 import paho.mqtt.client as mqtt
 import os
 
-def turn_on_led(*args, duration: float = 3):
-    """Turn on the LED for a given gpio pin"""
-    for pin in args:
-        GPIO.output(pin, GPIO.HIGH)
-    sleep(duration)
-    for pin in args:
-        GPIO.output(pin, GPIO.LOW)
-
 LED_RED_GPIO = 14
 LED_YELLOW_GPIO = 18
 LED_GREEN_GPIO = 24
@@ -23,49 +15,56 @@ GPIO.setup(LED_YELLOW_GPIO, GPIO.OUT, initial=GPIO.LOW)
 
 client = mqtt.Client()
 client.connect(os.environ['MQTT_BROKER_IP'], int(os.environ['MQTT_BROKER_PORT']))
+client.loop_forever()
 
 if "FALSE" == os.environ['TRAFFIC_LIGHT_IS_LEADER']:
     try:
         def on_connect(client, userdata, flags, rc, properties=None):
             client.subscribe("topic/lights")
         def on_message(client, userdata, msg):
+            GPIO.cleanup()
             if msg.payload.decode() == "red":
                 # Red phase
-                turn_on_led(LED_RED_GPIO, duration=14)
+                GPIO.output(LED_RED_GPIO, GPIO.HIGH)
             if msg.payload.decode() == "yellow":
                 # Yellow phase
-                turn_on_led(LED_YELLOW_GPIO, duration=2)
+                GPIO.output(LED_YELLOW_GPIO, GPIO.HIGH)
             if msg.payload.decode() == "green":
                 # Green phase
-                turn_on_led(LED_GREEN_GPIO, duration=10)
+                GPIO.output(LED_GREEN_GPIO, GPIO.HIGH)
             if msg.payload.decode() == "prepare":
-                # Prepare green 
-                turn_on_led(LED_RED_GPIO, LED_YELLOW_GPIO, duration=2)
+                # pre green 
+                GPIO.output(LED_RED_GPIO, GPIO.HIGH)
+                GPIO.output(LED_YELLOW_GPIO, GPIO.HIGH)
         client.on_connect = on_connect
         client.on_message = on_message
-        client.loop_forever()
-        client.disconnect();
     except KeyboardInterrupt:
       pass
 else:
   try:
     while True:
-        # Red phase
-        turn_on_led(LED_RED_GPIO, duration=14)
-        # Another traffic works
-        client.publish("topic/lights", "prepare")
+        # Red phase => Other green
         client.publish("topic/lights", "green")
+        GPIO.cleanup()
+        GPIO.output(LED_RED_GPIO, GPIO.HIGH)
+        sleep(5)
+                
+        # Prepare green => Other yellow
         client.publish("topic/lights", "yellow")
+        GPIO.cleanup()
+        GPIO.output(LED_RED_GPIO, GPIO.HIGH)
+        GPIO.output(LED_YELLOW_GPIO, GPIO.HIGH)
+        
+        # Green phase => Other red
         client.publish("topic/lights", "red")
-        # Prepare green
-        turn_on_led(LED_RED_GPIO, LED_YELLOW_GPIO, duration=2)
-        # Yellow phase
-        turn_on_led(LED_YELLOW_GPIO, duration=2)
-        # Green phase
-        turn_on_led(LED_GREEN_GPIO, duration=10)
-        client.disconnect();
+        GPIO.cleanup()
+        GPIO.output(LED_GREEN_GPIO, GPIO.HIGH)
+        
+        # Yellow phase => Other prepare 
+        client.publish("topic/lights", "prepare")
+        GPIO.cleanup()
+        GPIO.output(LED_YELLOW_GPIO, GPIO.HIGH)
   except KeyboardInterrupt:
-    print("Stopped")
     pass
   finally:
     GPIO.cleanup()
