54bfe29588377394c4723f7d5977f7cafa5805ac
WinchesterGirl <ekarou02@ucy.ac.cy>
2023-03-29 08:54:56 +0200

    make the subscriber script

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import RPi.GPIO as GPIO
 from time import sleep
+import paho.mqtt.client as mqtt
+
+#It's a subscriber
 
 def turn_on_led(gpio_num: int, duration: float = 3):
-    """Turn on the LED for a given gpio pin"""
+   """Turn on the LED for a given gpio pin"""
     GPIO.output(gpio_num, GPIO.HIGH)
     sleep(duration)
     GPIO.output(gpio_num, GPIO.LOW)
@@ -17,15 +20,26 @@ GPIO.setup(LED_GREEN_GPIO, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(LED_RED_GPIO, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(LED_YELLOW_GPIO, GPIO.OUT, initial=GPIO.LOW)
 
-try:
-    while True:
+def on_connect(client,userdata,rc):
+    client.subscribe("topic/lights")
+
+def on_message(client,userdata,msg):
+    if msg.payload.decode() == "red":
         turn_on_led(LED_RED_GPIO)
-        sleep(1)
+    if msg.payload.decode() == "yellow":
         turn_on_led(LED_YELLOW_GPIO)
-        sleep(1)
+    if msg.payload.decode() == "green":
         turn_on_led(LED_GREEN_GPIO)
-        sleep(1)              
+    client.disconnect();
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("192.168.0.213", 1883, 60)
+
+client.loop_forever()
+
 except KeyboardInterrupt:
     pass
 finally:
-    GPIO.cleanup()
+    GPIO.cleanup()    
